45d876a31ef5be8cefaa4f074a95a73806ba08ea
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-01-13 18:02:04 +0200

    Add files via upload

---
+++ frmLoan.designer.vb
@@ -0,0 +1,198 @@
+﻿Partial Public Class frmLoan
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.lblPrincipal = New System.Windows.Forms.Label()
+    Me.lblYearlyRate = New System.Windows.Forms.Label()
+    Me.lblYrs = New System.Windows.Forms.Label()
+    Me.btnPayment = New System.Windows.Forms.Button()
+    Me.btnAmort = New System.Windows.Forms.Button()
+    Me.btnRateTable = New System.Windows.Forms.Button()
+    Me.btnQuit = New System.Windows.Forms.Button()
+    Me.txtNumYears = New System.Windows.Forms.TextBox()
+    Me.txtPrincipal = New System.Windows.Forms.TextBox()
+    Me.txtYearlyRate = New System.Windows.Forms.TextBox()
+    Me.dgvResults = New System.Windows.Forms.DataGridView()
+    Me.btnShow = New System.Windows.Forms.Button()
+    CType(Me.dgvResults, System.ComponentModel.ISupportInitialize).BeginInit()
+    Me.SuspendLayout()
+    '
+    'lblPrincipal
+    '
+    Me.lblPrincipal.AutoSize = True
+    Me.lblPrincipal.Location = New System.Drawing.Point(67, 19)
+    Me.lblPrincipal.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lblPrincipal.Name = "lblPrincipal"
+    Me.lblPrincipal.Size = New System.Drawing.Size(107, 17)
+    Me.lblPrincipal.TabIndex = 0
+    Me.lblPrincipal.Text = "Amount of loan:"
+    '
+    'lblYearlyRate
+    '
+    Me.lblYearlyRate.AutoSize = True
+    Me.lblYearlyRate.Location = New System.Drawing.Point(85, 57)
+    Me.lblYearlyRate.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lblYearlyRate.Name = "lblYearlyRate"
+    Me.lblYearlyRate.Size = New System.Drawing.Size(88, 17)
+    Me.lblYearlyRate.TabIndex = 2
+    Me.lblYearlyRate.Text = "Interest rate:"
+    '
+    'lblYrs
+    '
+    Me.lblYrs.AutoSize = True
+    Me.lblYrs.Location = New System.Drawing.Point(27, 95)
+    Me.lblYrs.Margin = New System.Windows.Forms.Padding(5, 1, 5, 4)
+    Me.lblYrs.Name = "lblYrs"
+    Me.lblYrs.Size = New System.Drawing.Size(148, 17)
+    Me.lblYrs.TabIndex = 4
+    Me.lblYrs.Text = "Number of loan years:"
+    '
+    'btnPayment
+    '
+    Me.btnPayment.Location = New System.Drawing.Point(17, 150)
+    Me.btnPayment.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnPayment.Name = "btnPayment"
+    Me.btnPayment.Size = New System.Drawing.Size(243, 36)
+    Me.btnPayment.TabIndex = 6
+    Me.btnPayment.Text = "Calculate Monthly Payment"
+    '
+    'btnAmort
+    '
+    Me.btnAmort.Location = New System.Drawing.Point(16, 233)
+    Me.btnAmort.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnAmort.Name = "btnAmort"
+    Me.btnAmort.Size = New System.Drawing.Size(243, 36)
+    Me.btnAmort.TabIndex = 7
+    Me.btnAmort.Text = "Display Amortization Schedule"
+    '
+    'btnRateTable
+    '
+    Me.btnRateTable.Location = New System.Drawing.Point(17, 399)
+    Me.btnRateTable.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnRateTable.Name = "btnRateTable"
+    Me.btnRateTable.Size = New System.Drawing.Size(243, 36)
+    Me.btnRateTable.TabIndex = 8
+    Me.btnRateTable.Text = "Display Interest Rate Change Table"
+    '
+    'btnQuit
+    '
+    Me.btnQuit.Location = New System.Drawing.Point(17, 482)
+    Me.btnQuit.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnQuit.Name = "btnQuit"
+    Me.btnQuit.Size = New System.Drawing.Size(243, 36)
+    Me.btnQuit.TabIndex = 9
+    Me.btnQuit.Text = "Quit"
+    '
+    'txtNumYears
+    '
+    Me.txtNumYears.Location = New System.Drawing.Point(179, 92)
+    Me.txtNumYears.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtNumYears.Name = "txtNumYears"
+    Me.txtNumYears.Size = New System.Drawing.Size(79, 22)
+    Me.txtNumYears.TabIndex = 5
+    Me.txtNumYears.Text = "30"
+    '
+    'txtPrincipal
+    '
+    Me.txtPrincipal.Location = New System.Drawing.Point(179, 16)
+    Me.txtPrincipal.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtPrincipal.Name = "txtPrincipal"
+    Me.txtPrincipal.Size = New System.Drawing.Size(79, 22)
+    Me.txtPrincipal.TabIndex = 1
+    Me.txtPrincipal.Text = "250000"
+    '
+    'txtYearlyRate
+    '
+    Me.txtYearlyRate.Location = New System.Drawing.Point(179, 54)
+    Me.txtYearlyRate.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtYearlyRate.Name = "txtYearlyRate"
+    Me.txtYearlyRate.Size = New System.Drawing.Size(79, 22)
+    Me.txtYearlyRate.TabIndex = 3
+    Me.txtYearlyRate.Text = "4"
+    '
+    'dgvResults
+    '
+    Me.dgvResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+    Me.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+    Me.dgvResults.Location = New System.Drawing.Point(288, 15)
+    Me.dgvResults.Margin = New System.Windows.Forms.Padding(4)
+    Me.dgvResults.Name = "dgvResults"
+    Me.dgvResults.RowHeadersVisible = False
+    Me.dgvResults.Size = New System.Drawing.Size(560, 504)
+    Me.dgvResults.TabIndex = 10
+    '
+    'btnShow
+    '
+    Me.btnShow.Location = New System.Drawing.Point(16, 316)
+    Me.btnShow.Margin = New System.Windows.Forms.Padding(4)
+    Me.btnShow.Name = "btnShow"
+    Me.btnShow.Size = New System.Drawing.Size(243, 36)
+    Me.btnShow.TabIndex = 11
+    Me.btnShow.Text = "Show Interest Paid for One Year"
+    Me.btnShow.UseVisualStyleBackColor = True
+    '
+    'frmLoan
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(865, 530)
+    Me.Controls.Add(Me.btnShow)
+    Me.Controls.Add(Me.dgvResults)
+    Me.Controls.Add(Me.txtYearlyRate)
+    Me.Controls.Add(Me.txtPrincipal)
+    Me.Controls.Add(Me.txtNumYears)
+    Me.Controls.Add(Me.btnQuit)
+    Me.Controls.Add(Me.btnRateTable)
+    Me.Controls.Add(Me.btnAmort)
+    Me.Controls.Add(Me.btnPayment)
+    Me.Controls.Add(Me.lblYrs)
+    Me.Controls.Add(Me.lblYearlyRate)
+    Me.Controls.Add(Me.lblPrincipal)
+    Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.Name = "frmLoan"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Analyze a Loan"
+    CType(Me.dgvResults, System.ComponentModel.ISupportInitialize).EndInit()
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents lblPrincipal As System.Windows.Forms.Label
+  Friend WithEvents lblYearlyRate As System.Windows.Forms.Label
+  Friend WithEvents lblYrs As System.Windows.Forms.Label
+  Friend WithEvents btnPayment As System.Windows.Forms.Button
+  Friend WithEvents btnAmort As System.Windows.Forms.Button
+  Friend WithEvents btnRateTable As System.Windows.Forms.Button
+  Friend WithEvents btnQuit As System.Windows.Forms.Button
+  Friend WithEvents txtNumYears As System.Windows.Forms.TextBox
+  Friend WithEvents txtPrincipal As System.Windows.Forms.TextBox
+  Friend WithEvents txtYearlyRate As System.Windows.Forms.TextBox
+  Friend WithEvents dgvResults As System.Windows.Forms.DataGridView
+  Friend WithEvents btnShow As System.Windows.Forms.Button
+
+End Class
---
+++ frmLoan.vb
@@ -0,0 +1,147 @@
+﻿Public Class frmLoan
+
+  Structure Month
+    Dim number As Integer
+    Dim interestPaid As Decimal
+    Dim principalPaid As Decimal
+    Dim endBalance As Decimal
+  End Structure
+
+  Structure EffectOfRate
+    Dim interestRate As Decimal
+    Dim monthlyPayment As Decimal
+  End Structure
+
+  Private Sub btnPayment_Click(sender As System.Object, e As System.EventArgs) Handles btnPayment.Click
+    Dim principal As Decimal  'Amount of the loan
+    Dim yearlyRate As Decimal 'Annual rate of interest
+    Dim numMonths As Integer 'Number of months to repay loan
+    InputData(principal, yearlyRate, numMonths)
+    Dim monthlyRate As Decimal = yearlyRate / 12
+    Dim monthlyPayment As Decimal
+    'Calculate monthly payment
+    monthlyPayment = Payment(principal, monthlyRate, numMonths)
+    'Display results
+    MessageBox.Show("Monthly payment: " & monthlyPayment.ToString("C"), "Payment")
+  End Sub
+
+  Private Sub btnAmort_Click(sender As System.Object, e As System.EventArgs) Handles btnAmort.Click
+    Dim principal As Decimal  'amount of the loan
+    Dim yearlyRate As Decimal 'annual rate of interest
+    Dim numMonths As Integer 'number of months to repay loan
+    InputData(principal, yearlyRate, numMonths)
+    Dim months(numMonths - 1) As Month
+    Dim monthlyRate As Decimal = yearlyRate / 12
+    Dim monthlyPayment As Decimal = Payment(principal, monthlyRate, numMonths)
+    months = GenerateMonthsArray(principal, monthlyRate, numMonths)
+    Dim query = From mnth In months
+                Let num = mnth.number
+                Let interest = (mnth.interestPaid).ToString("C")
+                Let prin = (mnth.principalPaid).ToString("C")
+                Let bal = (mnth.endBalance).ToString("C")
+                Select num, interest, prin, bal
+    dgvResults.DataSource = query.ToList
+    dgvResults.CurrentCell = Nothing
+    dgvResults.Columns("num").HeaderText = "Month"
+    dgvResults.Columns("interest").HeaderText = "Amount Paid for Interest"
+    dgvResults.Columns("prin").HeaderText = "Amount Paid for Principal"
+    dgvResults.Columns("bal").HeaderText = "Balance at End of Month"
+  End Sub
+
+  Private Sub btnShow_Click(sender As System.Object, e As System.EventArgs) Handles btnShow.Click
+    Dim principal As Decimal  'amount of loan
+    Dim yearlyRate As Decimal 'annual rate of interest
+    Dim numMonths As Integer 'number of months to repay loan
+    InputData(principal, yearlyRate, numMonths)
+    Dim months(numMonths - 1) As Month
+    Dim monthlyRate As Decimal = yearlyRate / 12
+    Dim monthlyPayment As Decimal = Payment(principal, monthlyRate, numMonths)
+    months = GenerateMonthsArray(principal, monthlyRate, numMonths)
+    Dim beginningMonth As Integer = CInt(InputBox("Enter beginning month: "))
+    Dim query = From month In months
+                Where (month.number >= beginningMonth) And
+                      (month.number < beginningMonth + 12)
+                Select month.interestPaid
+    MessageBox.Show("Interest Paid for year beginning with month " &
+            beginningMonth & ": " & (query.Sum).ToString("C"), "Interest For Year")
+  End Sub
+
+  Private Sub btnRateTable_Click(sender As System.Object, e As System.EventArgs) Handles btnRateTable.Click
+    Dim principal As Decimal  'amount of loan
+    Dim yearlyRate As Decimal 'annual rate of interest
+    Dim numMonths As Integer 'number of months to repay loan
+    InputData(principal, yearlyRate, numMonths)
+    Dim rates(16) As EffectOfRate
+    Dim monthlyRate As Decimal = yearlyRate / 12
+    'Dim monthlyPayment As Decimal = Payment(principal, monthlyRate, numMonths)
+    'Fill rates array
+    For i As Integer = 0 To 16
+      rates(i).interestRate = (yearlyRate - 0.01D) + i * 0.00125D
+      rates(i).monthlyPayment = Payment(principal, rates(i).interestRate / 12, numMonths)
+    Next
+    Dim query = From rate In rates
+                Let annualRate = (rate.interestRate).ToString("P3")
+                Let monthlyPayment = (rate.monthlyPayment).ToString("C")
+                Select annualRate, monthlyPayment
+    dgvResults.DataSource = query.ToList
+    dgvResults.CurrentCell = Nothing
+    dgvResults.Columns("annualRate").HeaderText = "Interest Rate"
+    dgvResults.Columns("monthlyPayment").HeaderText = "Monthly Payment"
+  End Sub
+
+  Private Sub btnQuit_Click(sender As System.Object, e As System.EventArgs) Handles btnQuit.Click
+    Me.Close()
+  End Sub
+
+  Sub InputData(ByRef principal As Decimal,
+                ByRef yearlyRate As Decimal,
+                ByRef numMonths As Integer)
+    'Input loan amount, yearly rate of interest, and duration
+    Dim percentageRate As Decimal, numYears As Integer
+    principal = CDec(txtPrincipal.Text)
+    percentageRate = CDec(txtYearlyRate.Text)
+    yearlyRate = percentageRate / 100   'Convert interest rate to decimal form
+    numYears = CInt(txtNumYears.Text)
+    numMonths = numYears * 12           'Duration of loan in months
+  End Sub
+
+  Function Payment(principal As Decimal, monthlyRate As Decimal, numMonths As Integer) As Decimal
+    Dim estimate As Decimal          'Estimate of monthly payment
+    estimate = CDec(principal * monthlyRate / (1 - (1 + monthlyRate) ^ (-numMonths)))
+    ' Round the payment up if there are fractions of a cent
+    If estimate = Math.Round(estimate, 2) Then
+      Return estimate
+    Else
+      Return Math.Round(estimate + 0.005D, 2)
+    End If
+  End Function
+
+  Function GenerateMonthsArray(principal As Decimal, monthlyRate As Decimal, numMonths As Integer) As Month()
+    Dim months(numMonths - 1) As Month
+    'Fill the months array
+    Dim monthlyPayment As Decimal = Payment(principal, monthlyRate, numMonths)
+    'Assign values for first month
+    months(0).number = 1
+    months(0).interestPaid = monthlyRate * principal
+    months(0).principalPaid = monthlyPayment - months(0).interestPaid
+    'months(0).endBalance = (1 + monthlyRate) * principal - monthlyPayment
+    months(0).endBalance = principal - months(0).principalPaid
+    'Assign values for interior months
+    For i As Integer = 1 To numMonths - 2
+      months(i).number = i + 1
+      months(i).interestPaid = monthlyRate * months(i - 1).endBalance
+      months(i).principalPaid = monthlyPayment - months(i).interestPaid
+      'months(i).endBalance = (1 + monthlyRate) * months(i - 1).endBalance -
+      '                         monthlyPayment
+      months(i).endBalance = months(i - 1).endBalance - months(i).principalPaid
+    Next
+    'Assign values for last month
+    months(numMonths - 1).number = numMonths
+    months(numMonths - 1).interestPaid = monthlyRate *
+                                         months(numMonths - 2).endBalance
+    months(numMonths - 1).principalPaid = months(numMonths - 2).endBalance
+    months(numMonths - 1).endBalance = 0
+    Return months
+  End Function
+
+End Class
